69ac6ab853881f9240bbacd398e054305294c5e2
Janynne Gomes <janynnegomes@gmail.com>
2019-10-07 23:45:50 -0300

    Updates

---
+++ precache-manifest.c93dc1c8476a1aea047fc743fc04fded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dad669ac5927d3d1e7bbf8b091a2ac2",
+    "url": "/react-dash-stars-app/index.html"
+  },
+  {
+    "revision": "0abc2fe03ddfb3e4eafc",
+    "url": "/react-dash-stars-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76a0e81bed36a137faa5",
+    "url": "/react-dash-stars-app/static/js/2.bb183502.chunk.js"
+  },
+  {
+    "revision": "0abc2fe03ddfb3e4eafc",
+    "url": "/react-dash-stars-app/static/js/main.3f8442d5.chunk.js"
+  },
+  {
+    "revision": "98039c4a1de9b34b8b8c",
+    "url": "/react-dash-stars-app/static/js/runtime-main.b6ba8f85.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-dash-stars-app/static/media/logo.25bf045c.svg"
+  }
+]);
